2d392df93824f845492a5f71d85502143a19b934
MattAzerty <maguillaud@laposte.net>
2023-02-07 15:36:58 +0100

    AdventOfCode2021: 001-002

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,37 +2,45 @@ import java.io.File
 
 fun main() {
 
-//----------------------------------------//https://adventofcode.com/2015
-
+//----------------------------------------//https://adventofcode.com/2021
+/*
 //CODE001
-    val data01 = File("data/2015/data01.txt").readText()//limited for 2GB file size!
-    println("-----DAY001-----")
-    year2015.Data01.answerQuestion(data01)
+    val dataY2021D01 = File("data/2021/data01.txt").readLines()
+    println("-----YEAR2021DAY001-----")
+    year2021.Data01.answerQuestion(dataY2021D01)
+*/
+//CODE002
+    val dataY2021D02 = File("data/2021/data02.txt").readLines()
+    println("-----YEAR2021DAY002-----")
+    year2021.Data02.answerQuestion(dataY2021D02)
+
+//----------------------------------------//https://adventofcode.com/2015
+/*
+//CODE001
+    val dataY2015D01 = File("data/2015/data01.txt").readText()//limited for 2GB file size!
+    println("-----YEAR2015DAY001-----")
+    year2015.Data01.answerQuestion(dataY2015D01)
 
 //CODE002
-    val data02 = File("data/2015/data02.txt").readLines()//limited for 2GB file size!
-    println("-----DAY002-----")
-    year2015.Data02.answerQuestion(data02)
+    val dataY2015D02 = File("data/2015/data02.txt").readLines()//limited for 2GB file size!
+    println("-----YEAR2015DAY002-----")
+    year2015.Data02.answerQuestion(dataY2015D02)
 
 //CODE003
-    val data03 = File("data/2015/data03.txt").readText()
-    println("-----DAY003-----")
-    year2015.Data03.answerQuestion(data03)
+    val dataY2015D03 = File("data/2015/data03.txt").readText()
+    println("-----YEAR2015DAY003-----")
+    year2015.Data03.answerQuestion(dataY2015D03)
 
 //CODE004
-    val data04 = "ckczppom"
-    println("-----DAY004-----")
-    year2015.Data04.answerQuestion(data04)
+    val dataY2015D04 = "ckczppom"
+    println("-----YEAR2015DAY004-----")
+    year2015.Data04.answerQuestion(dataY2015D04)
 
 //CODE005
-    val data05 = File("data/2015/data05.txt").readLines()
-    println("-----DAY005-----")
-    year2015.Data05.answerQuestion(data05)
-
-
-//----------------------------------------//https://adventofcode.com/2021
-
-//CODE001
+    val dataY2015D05 = File("data/2015/data05.txt").readLines()
+    println("-----YEAR2015DAY005-----")
+    year2015.Data05.answerQuestion(dataY2015D05)
+*/
 
 }//End of main
 
